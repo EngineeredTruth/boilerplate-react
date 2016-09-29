8b8090508b3803af3f4a5c5188ce47f0d86c632c
Engineered Truth <Tangoforthemango@gmail.com>
2016-09-29 16:12:48 -0700

    saving boilerplate

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Navbar from './navbar/Navbar.js'
+
+ReactDOM.render(
+<div>
+  <Navbar />
+</div>
+  , document.querySelector('.container'));
---
+++ src/navbar/Navbar.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+export default class Navbar extends Component {
+  constructor(props){
+    super(props)
+  }
+
+  render = () => {
+    return(
+      <div>
+        <h1>Navbar</h1>
+      </div>
+    )
+  }
+
+}
